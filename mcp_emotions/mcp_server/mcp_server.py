287a5f8eec9eaccae05e85468bb48a1e6eac78d6
Daniel Gomez Morales <danielgm78@msn.com>
2025-06-17 11:10:42 -0700

    Added CORS filter for frontend

--- mcp_emotions/mcp_server/mcp_server.py
+++ mcp_emotions/mcp_server/mcp_server.py
@@ -30,6 +30,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_origins=["https://mcp-emotions.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
