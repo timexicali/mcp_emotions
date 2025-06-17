e3f75f7cbe4dd3b0d9b3d26ad2c263bf37fc50e3
Daniel Gomez Morales <danielgm78@msn.com>
2025-06-17 11:29:59 -0700

    fix cors error

--- mcp_emotions/mcp_server/mcp_server.py
+++ mcp_emotions/mcp_server/mcp_server.py
@@ -29,8 +29,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_origins=["https://mcp-emotions.vercel.app"],
+    allow_origins=[
+        "http://localhost:3000", 
+        "https://mcp-emotions.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
